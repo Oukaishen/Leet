ec9a0a4e94636f723602cc79f218cffd35416171
kaishen <kill4410@163.com>
2017-08-05 11:18:04 +0800

    Add files via upload
    #538 simple inorder Tree Travel problem

---
+++ src/leet538.java
@@ -0,0 +1,18 @@
+
+public class leet538 {
+	
+	int sum = 0;
+public TreeNode convertBST(TreeNode root) {
+       sub(root);
+       return root;
+    }
+
+public void sub(TreeNode root){
+	if(root == null ) return;
+	sub(root.right);
+	int tmp = root.val;
+	root.val += sum;
+	sum += tmp;
+	sub(root.left);
+}
+}
