d3ed16e50b8df24c7d6e4f5512776fba59e6d7b7
kaishen <kill4410@163.com>
2019-02-17 11:19:02 +0800

    add rand7

---
+++ src/algorithm/Leet470.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+public class Leet470 {
+//    public int rand10simple(){
+//        int oddEven = rand7();
+//        while(oddEven < 6) oddEven = rand7();
+//        int oneTofive = rand7();
+//        while(oneTofive > 5) oneTofive = rand7();
+//        return (oddEven & 1) == 0 ? oneTofive: oneTofive + 5;
+//    }
+//
+//    public int rand10(){
+//        int row = -1;
+//        int col = -1;
+//        int idx = -1;
+//        do{
+//            row = rand7();
+//            col = rand7();
+//            idx = row + (col - 1) * 7;
+//        }while(idx > 40);
+//
+//        return 1 + (idx - 1) % 10;
+//    }
+}
