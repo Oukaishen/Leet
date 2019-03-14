24ef1f7347fb2ae51386b0d93d84e39db0802527
kaishen <kill4410@163.com>
2019-03-14 23:04:07 +0800

    add 567

--- src/algorithm/Leet567.java
+++ src/algorithm/Leet567.java
@@ -1,4 +1,79 @@
 package algorithm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Leet567 {
+
+    public static void main(String [] args){
+
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        /*
+         * 思路2：将S1的字母统计出来放在hash中，对S2每个长度的字串判断。
+         * 牛逼，实现出来了。
+         * */
+        if(s1.length() > s2.length()){return false;}
+        int [] charArr = new int[26];
+        for(int i = 0 ; i<26; i++){
+            charArr[i] = 0;
+        }
+        for(int i = 0; i<s1.length(); i++){
+            charArr[ s1.charAt(i) - 'a' ] += 1;
+        }
+        for(int i = 0; i <= s2.length() - s1.length(); i++){
+            if(i == 0){
+                for(int j =0 ; j< s1.length(); j++){
+                    charArr[s2.charAt(j) - 'a'] -= 1;
+                }
+                if(checkArr(charArr)){
+                    return true;
+                }
+            }
+            else {
+                charArr[s2.charAt(i-1) - 'a'] += 1;
+                charArr[s2.charAt(i + s1.length() - 1) - 'a'] -=1;
+                if(checkArr(charArr)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkArr(int [] a){
+        for(int i = 0; i<a.length; i++){
+            if(a[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Set<String> permute;
+    public boolean checkInclusion1(String s1, String s2) {
+        /*
+        * 思路1：将S1的全排列找出来，然后存放在HASH中，对S2每个长度的字串判断命中。
+        * 但是显然，这个思路出现了TLE
+        * */
+        permute = new HashSet<>();
+        dfs("", s1);
+        for(int i = 0; i <= s2.length() - s1.length(); i++){
+            if(permute.contains(s2.substring(i,i+s1.length()))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void dfs(String prefix, String str){
+        if(str.equals("")){
+            permute.add(prefix);
+        }
+        for(int i = 0; i<str.length(); i++){
+            String newPrefix = prefix + str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            dfs(newPrefix, newStr);
+        }
+    }
 }
--- src/algorithm/Leet695.java
+++ src/algorithm/Leet695.java
@@ -1,4 +1,30 @@
 package algorithm;
 
 public class Leet695 {
+
+    boolean [][] visited;
+    int rows, cols;
+    public int maxAreaOfIsland(int[][] grid) {
+        if(grid == null ) return -1;
+        rows = grid.length;
+        cols = grid[0].length;
+        visited = new boolean[grid.length][grid[0].length];
+        int max = 0;
+        for(int r =0; r < rows; r++ ){
+            for(int c=0; c< cols; c++){
+                if(grid[r][c] == 1)
+                    max = Math.max(dfs(r,c,grid), max);
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int r, int c, int[][] grid){
+        if(r <0 || r >= rows || c<0 || c >= cols || grid[r][c] == 0  || visited[r][c]){
+            return 0;
+        }
+        visited[r][c] = true;
+        return 1 + dfs(r-1, c, grid) + dfs(r+1, c ,grid) +
+                dfs(r, c-1, grid) + dfs(r, c+1, grid);
+    }
 }
