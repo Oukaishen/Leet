d5405cded27d821ba3d59343d1d967a5a972e528
kaishenou on home pc <kill4410@163.com>
2021-06-03 22:57:06 +0800

    feat: 增加代码

--- src/algorithm/leet227.java
+++ src/algorithm/leet227.java
@@ -34,5 +34,93 @@ public class leet227 {
 		return res;
 }
 
+	public int calculate_new(String s){
+
+		if(null == s || s.isEmpty()){
+			return 0;
+		}
+		Stack<String> stack = new Stack<>();
+		int curNum = 0;
+		boolean curNumStart = false;
+		int ptr = 0;
+
+		while (ptr >= 0 && ptr < s.length()) {
+
+			char c = s.charAt(ptr);
+			if(c - '0' >= 0 && c -'0' <=9 ){
+				if(!curNumStart){
+					curNumStart = true;
+				}
+				curNum = curNum * 10 + (c - '0');
+				ptr++;
+			} else if( c == '+' || c == '-'){
+				if(curNumStart){
+					stack.push(String.valueOf(curNum));
+					curNum = 0;
+					curNumStart =false;
+				}
+				stack.push(String.valueOf(c));
+				ptr++;
+			} else if( c == '*' || c== '/'){
+				if(curNumStart){
+					stack.push(String.valueOf(curNum));
+					curNum = 0;
+					curNumStart =false;
+				}
+				int nextNum = 0;
+				boolean nextNumStart = false;
+				while(ptr < s.length()){
+					char nextc = s.charAt(ptr);
+					if( (nextc -'0' < 0 || nextc-'0' > 9) && nextNumStart){
+						break;
+					}else if(nextc -'0' >= 0 && nextc-'0' <= 9){
+						if(!nextNumStart){
+							nextNumStart = true;
+						}
+						nextNum = nextNum * 10 + (nextc -'0');
+					}
+					ptr++;
+				}
+				switch (c){
+					case '*': {
+						int lastNum = Integer.valueOf(stack.pop());
+						stack.push(String.valueOf(lastNum * nextNum));
+						break;
+					}case '/': {
+						int lastNum = Integer.valueOf(stack.pop());
+						stack.push(String.valueOf(lastNum / nextNum));
+					}
+				}
+			} else if( c == ' '){
+				if(curNumStart){
+					stack.push(String.valueOf(curNum));
+					curNum = 0;
+					curNumStart = false;
+				}
+				ptr++;
+			}
+
+		}
+
+		if(curNumStart){
+			stack.push(String.valueOf(curNum));
+			curNum = 0;
+			curNumStart = false;
+		}
+
+		int res = 0;
+		while(!stack.isEmpty()){
+			curNum = Integer.valueOf(stack.pop());
+			if(!stack.isEmpty() && stack.pop().equals("-")){
+				curNum = -curNum;
+			}
+			res += curNum;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new leet227().calculate_new("0*1+ 2* 3"));
+	}
 
 }
--- src/algorithm/leet239.java
+++ src/algorithm/leet239.java
@@ -1,9 +1,7 @@
 package algorithm;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class leet239 {
@@ -13,11 +11,13 @@ public class leet239 {
 	  
 	  int nums [] = {1,3,-1,-3,5,3,6,7};
 	  int k = 3;
-	  System.out.println(test.maxSlidingWindow(nums, k));
+	  List<Integer> res = Arrays.stream(test.maxSlidingWindow(nums, k)).boxed().collect(Collectors.toList());
+	  System.out.println(res);
+
 	}
 	// this method is called montonic queue. it has deep thought
 	//  redo on August 12th
-	public int[] maxSlidingWindow(int[] nums, int k) {
+	public int[] maxSlidingWindowBak(int[] nums, int k) {
 		if ( nums == null || k ==0 ) return new int[0];
 		
 		int n = nums.length;
@@ -40,4 +40,33 @@ public class leet239 {
 		}
 		return res;
     }
+
+  public int[] maxSlidingWindow(int[] nums, int k){
+
+		if(nums == null || k == 0){
+			return new int[0];
+		}
+
+		int N = nums.length;
+		int [] res = new int[N - k + 1];
+		int ptr = 0;
+
+		Deque<Integer> deque = new LinkedList<>();
+
+		for(int i = 0; i < N; i++){
+			while(!deque.isEmpty() && deque.peek() < i - k + 1 ){
+				deque.poll();
+			}
+			while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i] ){
+				deque.pollLast();
+			}
+			deque.offer(i);
+
+			if(i - k + 1 >= 0){
+				res[ptr++] = nums[deque.peek()];
+			}
+		}
+
+		return res;
+	}
 }
